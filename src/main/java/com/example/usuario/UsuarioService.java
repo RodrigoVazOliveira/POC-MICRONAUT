666c910b563af8432df0ae854decb3ea18bfb865
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-05-17 23:24:54 -0300

    Criar método para buscar usuário por id

--- src/main/java/com/example/usuario/UsuarioService.java
+++ src/main/java/com/example/usuario/UsuarioService.java
@@ -5,6 +5,7 @@ import io.micronaut.runtime.context.scope.ThreadLocal;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.Optional;
 
 @Singleton
 public class UsuarioService {
@@ -14,5 +15,19 @@ public class UsuarioService {
 
     public Usuario cadastrarNovoUsuario(Usuario novoUsuario) {
         return usuarioRepository.save(novoUsuario);
+    }
+
+    public Usuario atualizarTodoUsuario(Usuario usuario) {
+
+    }
+
+    public Usuario procurarUsuarioPeloId(Long id) {
+        Optional<Usuario> optionalUsuario = usuarioRepository.findById(id);
+
+        if (optionalUsuario.isEmpty()) {
+            throw new RuntimeException("Usuário com id " + id + " não localizado!");
+        }
+
+        return optionalUsuario.get();
     }
 }
